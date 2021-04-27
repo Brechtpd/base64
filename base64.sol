e2ecc20129a2d12f7f8e7944839ad5813ceb1b40
Brecht Devos <Brechtp.Devos@gmail.com>
2021-04-27 06:03:15 +0200

    Update base64.sol

--- base64.sol
+++ base64.sol
@@ -12,39 +12,49 @@ contract Base64 {
         // multiply by 4/3 rounded up
         uint256 encodedLen = 4 * ((data.length + 2) / 3);
 
-        // Add some extra buffer at the end
+        // add some extra buffer at the end required for the writing
         bytes memory result = new bytes(encodedLen + 32);
         
+        // prepare the lookup table
         bytes memory table = TABLE;
         uint tablePtr;
         assembly {
             tablePtr := add(table, 1)
         }
         
-        uint outOffset = 32;
-        for (uint i = 0; i < data.length;) {
-            i += 3;
-            uint input;
-            assembly {
-                input := and(mload(add(data, i)), 0xffffff)
-            }
-            
-            assembly {
-               let out := and(mload(add(tablePtr, and(shr(18, input), 0x3F))), 0xFF)
-               out := shl(8, out)
-               out := add(out, and(mload(add(tablePtr, and(shr(12, input), 0x3F))), 0xFF))
-               out := shl(8, out)
-               out := add(out, and(mload(add(tablePtr, and(shr(6, input), 0x3F))), 0xFF))
-               out := shl(8, out)
-               out := add(out, and(mload(add(tablePtr, and(input, 0x3F))), 0xFF))
-               out := shl(224, out)
-               
-               mstore(add(result, outOffset), out)
-            }
-            outOffset += 4;
+        // input ptr
+        uint dataPtr;
+        assembly {
+            dataPtr := data
         }
-
-        // Padding
+        uint endPtr = dataPtr + data.length;
+        
+        // result ptr, jump over length
+        uint resultPtr = 32;
+        assembly {
+            resultPtr := add(result, 32)
+        }
+        
+        // run over the input, 3 bytes at a time
+        while(dataPtr < endPtr) {
+            dataPtr += 3;
+            assembly {
+               // read 3 bytes
+               let input := mload(dataPtr)
+               
+               // write 4 characters
+               mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr(18, input), 0x3F)))))
+               resultPtr := add(resultPtr, 1)
+               mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr(12, input), 0x3F)))))
+               resultPtr := add(resultPtr, 1)
+               mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr( 6, input), 0x3F)))))
+               resultPtr := add(resultPtr, 1)
+               mstore(resultPtr, shl(248, mload(add(tablePtr, and(        input,  0x3F)))))
+               resultPtr := add(resultPtr, 1)
+            }
+        }
+        
+        // padding
         uint r = data.length % 3;
         if (r != 0) {
             r = (r == 1) ? 2 : 1;
